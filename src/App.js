831ba4afc624c6be794ffa96cff7a15b7b0d5a87
Alexander Brook <redlesleys@gmail.com>
2020-04-10 19:54:30 +0300

    added structure of other pages and header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+        <Header/>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
     </div>
   );
 }
